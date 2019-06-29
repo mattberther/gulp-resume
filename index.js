11c61a7cb2a656a91cdf60226aff1f90fd9e884f
Matt Berther <matt@berther.io>
2019-06-29 14:53:41 -0600

    update to meet eslint guidelines

--- index.js
+++ index.js
@@ -11,77 +11,85 @@ const THEME_REGISTRY = 'http://themes.jsonresume.org/themes.json';
 const SUPPORTED_FORMATS = ['html'];
 let SUPPORTED_THEMES = [];
 const DEFAULT_THEMES = [
-  'elegant', 'paper', 'kendall', 'flat',
-  'modern', 'classy', 'class', 'short',
-  'slick', 'kwan', 'onepage'
+    'elegant',
+    'paper',
+    'kendall',
+    'flat',
+    'modern',
+    'classy',
+    'class',
+    'short',
+    'slick',
+    'kwan',
+    'onepage'
 ];
 
 const getThemes = function (options, cb) {
-  options.url = THEME_REGISTRY;
-  request(options, function (err, resp, body) {
-    const themes = Object.keys(JSON.parse(body).themes) || DEFAULT_THEMES;
-    cb(themes, err);
-  });
+    options.url = THEME_REGISTRY;
+    request(options, function (err, resp, body) {
+        const themes = Object.keys(JSON.parse(body).themes) || DEFAULT_THEMES;
+        cb(themes, err);
+    });
 };
 
 module.exports = function (options) {
-  options = options || {};
+    options = options || {};
 
-  const format = options.format || 'html';
-  const theme = options.theme || 'flat';
-  const proxy = options.proxy || null;
+    const format = options.format || 'html';
+    const theme = options.theme || 'flat';
+    const proxy = options.proxy || null;
 
-  const stream = through.obj(function (file, enc, cb) {
-    const _self = this;
+    const stream = through.obj(function (file, enc, cb) {
+        const _self = this;
 
-    if (SUPPORTED_FORMATS.indexOf(format) === -1) {
-      this.emit('error', new PluginError(PLUGIN_NAME, 'invalid format specified'));
-      return cb();
-    }
-
-    const requestOptions = {};
-
-    if (proxy) {
-      requestOptions.proxy = proxy;
-    }
-
-    getThemes(requestOptions, function (themes) {
-      SUPPORTED_THEMES = themes;
-      if (SUPPORTED_THEMES.indexOf(theme) === -1) {
-        _self.emit('error', new PluginError(PLUGIN_NAME, 'invalid theme specified'));
-        return cb();
-      }
-    });
-
-    if (file.isBuffer()) {
-      request.post({
-        url: THEME_SERVER + theme,
-        proxy: proxy,
-        body: {
-          resume: JSON.parse(file.contents.toString())
-        },
-        json: true
-      }, function (err, resp, body) {
-        if (err) {
-          _self.emit('error', err);
-          return cb();
+        if (SUPPORTED_FORMATS.indexOf(format) === -1) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'invalid format specified'));
+            return cb();
         }
 
-        file.contents = new Buffer(body);
-        _self.push(file);
-        return cb();
-      });
-    }
+        const requestOptions = {};
 
-    if (file.isStream()) {
-      file.contents = file.contents.pipe(request.post({
-        url: THEME_SERVER + theme
-      }));
+        if (proxy) {
+            requestOptions.proxy = proxy;
+        }
 
-      this.push(file);
-      cb();
-    }
-  });
+        getThemes(requestOptions, function (themes) {
+            SUPPORTED_THEMES = themes;
+            if (SUPPORTED_THEMES.indexOf(theme) === -1) {
+                _self.emit('error', new PluginError(PLUGIN_NAME, 'invalid theme specified'));
+                return cb();
+            }
+        });
 
-  return stream;
+        if (file.isBuffer()) {
+            request.post({
+                url: THEME_SERVER + theme,
+                proxy: proxy,
+                body: {
+                    resume: JSON.parse(file.contents.toString())
+                },
+                json: true
+            }, function (err, resp, body) {
+                if (err) {
+                    _self.emit('error', err);
+                    return cb();
+                }
+
+                file.contents = Buffer.from(body);
+                _self.push(file);
+                return cb();
+            });
+        }
+
+        if (file.isStream()) {
+            file.contents = file.contents.pipe(request.post({
+                url: THEME_SERVER + theme
+            }));
+
+            this.push(file);
+            cb();
+        }
+    });
+
+    return stream;
 };
--- test/index.js
+++ test/index.js
@@ -14,83 +14,83 @@ const proxy = null; // Replace with string if you want to test with a proxy
 chai.use(require('chai-string'));
 
 describe('gulp-resume', function () {
-  let fakeResume;
-  this.timeout(3000);
+    let fakeResume;
+    this.timeout(3000);
 
-  function getFakeResume(fileContent) {
-    return new VinylFile({
-      path: './test/fixture/resume.json',
-      cwd: './test/',
-      base: './test/fixture/',
-      contents: new Buffer(fileContent || '')
-    });
-  }
+    function getFakeResume(fileContent) {
+        return new VinylFile({
+            path: './test/fixture/resume.json',
+            cwd: './test/',
+            base: './test/fixture/',
+            contents: Buffer.from(fileContent || '')
+        });
+    }
 
-  function getFakeFileReadStream() {
-    return new VinylFile({
-      contents: es.readArray(['Hello world']),
-      path: './test/fixture/resume.json'
-    });
-  }
+    function getFakeFileReadStream() {
+        return new VinylFile({
+            contents: es.readArray(['Hello world']),
+            path: './test/fixture/resume.json'
+        });
+    }
 
-  beforeEach(function () {
-    fakeResume = getFakeResume(fs.readFileSync('./test/fixtures/resume.json'));
-  });
-
-  describe('resume()', function () {
-    it('should create an html formatted resume', function (done) {
-      const stream = resume({theme: 'elegant', proxy: proxy});
-
-      stream.once('data', function (file) {
-        expect(file.isBuffer()).to.be.true;
-        expect(file.contents).to.exist;
-        expect(file.contents.toString()).to.startWith('<!DOCTYPE html><html lang="en">');
-        done();
-      });
-
-      stream.write(fakeResume);
-      stream.end();
+    beforeEach(function () {
+        fakeResume = getFakeResume(fs.readFileSync('./test/fixtures/resume.json'));
     });
 
-    it('should create an html formatted resume (stream)', function (done) {
-      const stream = resume({theme: 'elegant', proxy: proxy});
+    describe('resume()', function () {
+        it('should create an html formatted resume', function (done) {
+            const stream = resume({theme: 'elegant', proxy: proxy});
 
-      stream.once('data', function (file) {
-        expect(file.isStream()).to.be.true;
-        file.contents.pipe(es.wait(function (err, data) {
-          if (err) {
-            done(err);
-          }
+            stream.once('data', function (file) {
+                expect(file.isBuffer()).to.be.true;
+                expect(file.contents).to.exist;
+                expect(file.contents.toString()).to.startWith('<!DOCTYPE html><html lang="en">');
+                done();
+            });
 
-          expect(data.toString('utf8')).to.startWith('<!DOCTYPE html><html lang="en">');
-          done();
-        }));
-      });
+            stream.write(fakeResume);
+            stream.end();
+        });
 
-      stream.write(getFakeFileReadStream());
-      stream.end();
+        it('should create an html formatted resume (stream)', function (done) {
+            const stream = resume({theme: 'elegant', proxy: proxy});
+
+            stream.once('data', function (file) {
+                expect(file.isStream()).to.be.true;
+                file.contents.pipe(es.wait(function (err, data) {
+                    if (err) {
+                        done(err);
+                    }
+
+                    expect(data.toString('utf8')).to.startWith('<!DOCTYPE html><html lang="en">');
+                    done();
+                }));
+            });
+
+            stream.write(getFakeFileReadStream());
+            stream.end();
+        });
+
+        it('should validate the theme requested', function (done) {
+            const stream = resume({theme: 'invalid', proxy: proxy});
+            stream.once('error', function (err) {
+                expect(err).to.have.property('message', 'invalid theme specified');
+                done();
+            });
+
+            stream.write(fakeResume);
+            stream.end();
+        });
+
+        it('should error if the format is not html or pdf', function (done) {
+            const stream = resume({format: 'invalid', proxy: proxy});
+            stream.once('error', function (err) {
+                expect(err).to.have.property('message', 'invalid format specified');
+                done();
+            });
+
+            stream.write(fakeResume);
+            stream.end();
+        });
     });
-
-    it('should validate the theme requested', function (done) {
-      const stream = resume({theme: 'invalid', proxy: proxy});
-      stream.once('error', function (err) {
-        expect(err).to.have.property('message', 'invalid theme specified');
-        done();
-      });
-
-      stream.write(fakeResume);
-      stream.end();
-    });
-
-    it('should error if the format is not html or pdf', function (done) {
-      const stream = resume({format: 'invalid', proxy: proxy});
-      stream.once('error', function (err) {
-        expect(err).to.have.property('message', 'invalid format specified');
-        done();
-      });
-
-      stream.write(fakeResume);
-      stream.end();
-    });
-  });
 });
